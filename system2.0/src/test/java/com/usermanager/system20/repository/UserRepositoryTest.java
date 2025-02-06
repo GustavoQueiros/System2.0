8fc2fe5f3a1002298b404080ed1fc137116f7e72
GustavoQueiros <gustavo.dev47@gmail.com>
2025-02-06 18:17:16 -0300

    feat: create User Repository Test

---
+++ system2.0/src/test/java/com/usermanager/system20/repository/UserRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.usermanager.system20.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void existsByEmail() {
+        
+    }
+}
